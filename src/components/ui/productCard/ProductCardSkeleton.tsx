707ffec2fd717f5ef0186bc4596e9ed79c778d77
Tirtho-Ray <tirthoray10@gmail.com>
2025-10-22 23:52:25 +0600

    add good looking product card skeleton

---
+++ src/components/ui/productCard/ProductCardSkeleton.tsx
@@ -0,0 +1,25 @@
+
+
+const ProductCardSkeleton = () => {
+    return (
+        <div className="relative bg-white rounded-xl md:rounded-2xl shadow-2xl overflow-hidden font-sans h-[200px] w-[160px] md:h-[290px] md:w-[190px] lg:h-[350px] lg:w-[250px] animate-pulse">
+            <div className="h-[100px] md:h-[160px] lg:h-[200px] bg-gray-200" />
+
+            <div className="p-2 md:p-3 space-y-2">
+                <div className="h-4 bg-gray-300 rounded w-3/4" />
+                <div className="flex items-center space-x-2">
+                    <div className="h-4 w-10 bg-gray-300 rounded" />
+                    <div className="h-3 w-8 bg-gray-200 rounded" />
+                </div>
+                <div className="h-3 w-12 bg-gray-300 rounded" />
+                <div className="mt-2">
+                    <div className="h-3 w-10 bg-gray-300 rounded mb-1" />
+                    <div className="h-3 w-16 bg-gray-200 rounded" />
+                </div>
+                <div className="mt-2 h-3 w-24 bg-gray-200 rounded" />
+            </div>
+        </div>
+    );
+};
+
+export default ProductCardSkeleton;
