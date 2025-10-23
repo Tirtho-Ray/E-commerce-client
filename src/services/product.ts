0314949ce702c8c6dd3d4bbf321822d2217c6d2b
Tirtho-Ray <tirthoray10@gmail.com>
2025-10-23 21:05:17 +0600

    ready for search product

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { axiosInstance } from "../api/baseApi/axiosInstance"
 
- export const getProduct  = async () =>{
-    try {
-        const response  = await axiosInstance.get("/product");
-        return response.data
-        
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error:any) {
-       throw error.response?.data ||error
-    }
-}
+export const getProduct = async (search?: string) => {
+  try {
+    const query = search ? `?search=${encodeURIComponent(search)}` : "";
+    const response = await axiosInstance.get(`/products${query}`);
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error;
+  }
+};
+
+export const getProductDetails = async (id: string) => {
+  try {
+     if (!id) throw new Error("Product ID is required!");
+    const response = await axiosInstance.get(`/products/${id}`);
+    // console.log(response.data.data)
+    return response.data; 
+  } catch (error: any) {
+    throw error.response?.data || error;
+  }
+};
