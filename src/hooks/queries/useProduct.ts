19fee7fb6992ee7de60b1ceeef44f5de56065626
Tirtho-Ray <tirthoray10@gmail.com>
2025-10-23 21:04:43 +0600

    ready for search product

--- src/hooks/queries/useProduct.ts
+++ src/hooks/queries/useProduct.ts
@@ -1,9 +1,25 @@
 import { useQuery } from "@tanstack/react-query"
-import { getProduct } from "../../services/product"
+import { getProduct, getProductDetails } from "../../services/product"
 
-export const useProduct = () =>{
-    return useQuery({
-        queryKey:["product"],
-        queryFn:() =>getProduct()
-    })
-}
+export const useProduct = (search?: string) => {
+  return useQuery({
+    queryKey: ["product", search],
+    queryFn: () => getProduct(search),
+    staleTime: 30 * 1000,
+    gcTime: 5 * 60 * 1000,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    enabled: search !== undefined,
+  });
+};
+
+export const useProductDetails = (id: string) => {
+  return useQuery({
+    queryKey: ["productDetails", id],
+    queryFn: () => getProductDetails(id),
+    enabled: !!id, 
+    staleTime: 1000 * 60,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  });
+};
